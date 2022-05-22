3d09f2f4b86ba9965913341dce84c6b04fe24340
BeomChul Kim <2017253083@yonsei.ac.kr>
2022-05-22 23:05:39 +0900

    add
    addd

--- webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/GenericFileProvider.java
+++
@@ -1,7 +0,0 @@
-package io.flutter.plugins.webviewflutter;
-
-import androidx.core.content.FileProvider;
-
-public class GenericFileProvider extends FileProvider {
-    public static final String PROVIDER_NAME = "io.flutter.plugins.webviewflutter.GenericFileProvider";
-}
